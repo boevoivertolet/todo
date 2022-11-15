af954be91a32e163c73199ef12c0ea83f338939f
boevoivertolet <bjha78rus@yandex.ru>
2022-11-15 15:30:54 +0300

    2 less

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 function App() {
+
+    const tasks1: Array<TaskType> = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+    const tasks2: Array<TaskType> = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: '1917', isDone: false}
+    ]
+
     return (
         <div className="App">
-           <Todolist/>
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'Movies'} tasks={tasks2}/>
         </div>
     );
 }
