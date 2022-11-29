ed08054bd5fc888358c40467f989d8c267023cb2
boevoivertolet <bjha78rus@yandex.ru>
2022-11-29 13:13:21 +0300

    decorators: [ReduxStoreProviderDecorator]

--- src/AppWithRedux.stories.tsx
+++ src/AppWithRedux.stories.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {AppWithRedux} from './AppWithRedux';
-import {Provider} from 'react-redux';
-import {store} from './state/store';
+import {ReduxStoreProviderDecorator} from './stories/ReduxStoreProviderDecorator';
 
 
 export default {
     title: 'AppWithRedux Component',
     component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
 }
 
 
 export const AppWithReduxBaseExample = () => {
-    return <Provider store={store}>
-        <AppWithRedux/>
-    </Provider>
+    return <AppWithRedux/>
 }
--- src/stories/ReduxStoreProviderDecorator.tsx
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -1,6 +1,6 @@
 import {Provider} from 'react-redux';
 import {store} from '../state/store';
 
-export const ReduxStoreProviderDecorator = (story: any) => {
-    return <Provider store={store}>story()</Provider>
+export const ReduxStoreProviderDecorator = (storyFn: any) => {
+    return <Provider store={store}>{storyFn()}</Provider>
 }
